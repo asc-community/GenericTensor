fa41aa04dc23aaa40e35870c34828d4025cd22de
Angourisoft <lev.brovchenkov@gmail.com>
2020-07-22 21:25:36 +0300

    Benchmark

---
+++ Benchmark/Matrix.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using GenericTensor.Functions;
+using GenericTensor.Core;
+
+namespace Benchmark
+{
+    using TS = Tensor<TensorIntWrapper, int>;
+
+    public class MatrixBenchmark
+    {
+        static TS CreateMatrix(int size)
+            => TS.CreateMatrix(size, size, (k) =>
+                {
+                    var res = new TensorIntWrapper();
+                    res.SetValue(k.x + k.y);
+                    return res;
+                }
+        );
+
+        [Params(3, 20, 50)] public int MatrixSize;
+
+        [Benchmark] public void CreatingMatrix()
+            => CreateMatrix(MatrixSize);
+
+        [Benchmark] public void CreatingMatrixAndTranspose()
+            => CreateMatrix(MatrixSize).TransposeMatrix();
+
+        [Params(2, 4, 6)] public int LowMatrixSize;
+
+        [Benchmark] public void CreatingMatrixAndLaplace()
+            => CreateMatrix(LowMatrixSize).DeterminantLaplace();
+
+        [Benchmark] public void CreatingMatrixAndGaussian()
+            => CreateMatrix(LowMatrixSize).DeterminantGaussianSafeDivision();
+
+        [Benchmark] public void CreatingMatrixAndMultiply()
+            => TS.MatrixDotProduct(CreateMatrix(MatrixSize), CreateMatrix(MatrixSize));
+
+        [Benchmark] public void CreatingMatrixAndAdd()
+            => TS.PiecewiseAdd(CreateMatrix(MatrixSize), CreateMatrix(MatrixSize));
+    }
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MatrixBenchmark>();
+        }
+    }
+}
