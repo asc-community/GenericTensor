6b37749e7a2d60e86362ddbbeb396a145b9c5a18
Angourisoft <lev.brovchenkov@gmail.com>
2020-07-24 22:58:52 +0300

    Binary power added

---
+++ GenericTensor/Functions/Power.cs
@@ -0,0 +1,46 @@
+﻿namespace GenericTensor.Core
+{
+    public partial class GenTensor<T>
+    {
+        /// <summary>
+        /// Binary power
+        /// 
+        /// n positive:
+        /// A ^ n = A * A * ... * A
+        ///
+        /// n negative:
+        /// A ^ n = (A^(-1) * A^(-1) * ... * A^(-1))
+        ///
+        /// n == 0:
+        /// A ^ n = I
+        ///
+        /// O(log(power) * N^3)
+        /// </summary>
+        public static GenTensor<T> MatrixPower(GenTensor<T> m, int power)
+        {
+            #if ALLOW_EXCEPTIONS
+            if (!m.IsSquareMatrix)
+                throw new InvalidShapeException("Square matrix required");
+            #endif
+            if (power == 0)
+                return CreateIdentityMatrix(m.Shape.shape[0]);
+            if (power < 0)
+            {
+                m = m.Forward();
+                m.InvertMatrix();
+                power *= -1;
+            }
+            if (power == 1)
+                return m;
+            if (power == 2)
+                return MatrixDotProduct(m, m);
+            var half = power / 2;
+            var m1 = MatrixPower(m, half);
+            var dotted = MatrixDotProduct(m1, m1);
+            if (power % 2 == 0)
+                return dotted;
+            else
+                return MatrixDotProduct(dotted, m);
+        }
+    }
+}
---
+++ UnitTests/Power.cs
@@ -0,0 +1,82 @@
+﻿#region copyright
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2020 WhiteBlackGoose
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#endregion
+
+
+using System;
+using GenericTensor.Core;
+using GenericTensor.Functions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    using TS = GenTensor<int>;
+
+    [TestClass]
+    public class Power
+    {
+        public Power()
+        {
+            BuiltinTypeInitter.InitForFloat();
+        }
+
+
+        [TestMethod]
+        public void Test1()
+        {
+            var M = GenTensor<float>.CreateMatrix(new float[,]
+            {
+                {6,  1, 1},
+                {4, -2, 5},
+                {2,  8, 7}
+            });
+            var powered = GenTensor<float>.MatrixPower(M, 4);
+            var I = GenTensor<float>.CreateIdentityMatrix(3);
+            for (int i = 0; i < 4; i++)
+                I = GenTensor<float>.MatrixDotProduct(I, M);
+
+            Assert.AreEqual(I, powered);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var M = GenTensor<float>.CreateMatrix(new float[,]
+            {
+                {6,  1, 1},
+                {4, -2, 5},
+                {2,  8, 7}
+            });
+            var powered = GenTensor<float>.MatrixPower(M, -4);
+            M = M.Forward();
+            M.InvertMatrix();
+            var I = GenTensor<float>.CreateIdentityMatrix(3);
+            for (int i = 0; i < 4; i++)
+                I = GenTensor<float>.MatrixDotProduct(I, M);
+
+            Assert.AreEqual(I, powered);
+        }
+    }
+}
