0bc0b88b586ed27d7f4de676ba19ae838249d403
WhiteBlackGoose <whiteblackgoose@asc-community.org>
2020-10-18 17:22:42 +0300

    Codecov >:| (#15)

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,33 +1,35 @@
-﻿#region copyright
-/*
- * MIT License
- * 
- * Copyright (c) 2020 WhiteBlackGoose
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-#endregion
-
-using System;
+﻿using System;
 using GenericTensor.Core;
 using GenericTensor.Functions;
 using Sample;
+
+var M = GenTensor<double, DoubleWrapper>.CreateMatrix(new double[,]
+{
+    { -3,  0,  0,  0,   1,  -130 },
+    {  0, -2,  1,  0,   0,   -39 },
+    {  0,  0, -3,  2,   0,     0 },
+    {  0,  0,  0, -7,   3,  -273 },
+    {  0,  0,  0,  0, -26, -1729 }
+}
+);
+
+Console.WriteLine(M.GaussianEliminationSafeDivision());
+return;
+
+var A = new MyMatrix(3, 2);
+A[0, 0] = 5; A[0, 1] = 6;
+A[1, 0] = 3; A[1, 1] = 8;
+A[2, 0] = 2; A[2, 1] = 9;
+
+var B = new MyMatrix(2, 2);
+B[0, 0] = 5; B[1, 0] = 6;
+B[0, 1] = 3; B[1, 1] = 8;
+
+Console.WriteLine(A);
+Console.WriteLine("\nmultiplied by \n");
+Console.WriteLine(B);
+Console.WriteLine("\nis \n");
+Console.WriteLine(A * B);
 
 public static class Samples
 {
@@ -67,27 +69,5 @@ public static class Samples
             GenTensor<T, GenericWrapper<T>>.CreateMatrix(a),
             GenTensor<T, GenericWrapper<T>>.CreateMatrix(b)
         );
-    }
-}
-
-
-class Program
-{
-    static void Main(string[] _)
-    {
-        var A = new MyMatrix(3, 2);
-        A[0, 0] = 5; A[0, 1] = 6;
-        A[1, 0] = 3; A[1, 1] = 8;
-        A[2, 0] = 2; A[2, 1] = 9;
-
-        var B = new MyMatrix(2, 2);
-        B[0, 0] = 5; B[1, 0] = 6;
-        B[0, 1] = 3; B[1, 1] = 8;
-
-        Console.WriteLine(A);
-        Console.WriteLine("\nmultiplied by \n");
-        Console.WriteLine(B);
-        Console.WriteLine("\nis \n");
-        Console.WriteLine(A * B);
     }
 }
