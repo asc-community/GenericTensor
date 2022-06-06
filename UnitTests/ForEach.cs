eacf463005d27a376d2360f55e4b56cad3c4abea
WhiteBlackGoose <wbg@angouri.org>
2022-06-06 09:57:30 +0300

    Update UnitTests/ForEach.cs
    Co-authored-by: Hadrian Tang <hadrianwttang@outlook.com>

--- UnitTests/ForEach.cs
+++ UnitTests/ForEach.cs
@@ -36,7 +36,7 @@ namespace UnitTests
         {
             var tensor = GenTensor<int, IntWrapper>.CreateTensor(new[,,] { { { 1, 2, 3 }, { 4, 5, 6 } }, { { 7, 8, 9 }, { 10, 11, 12 } } });
             tensor.ForEach(new CapturingValueAction<int[], int, GenTensor<int, IntWrapper>>((index, value, t) => t[index] = value * value, tensor));
-            var expected = GenTensor<int, IntWrapper>.CreateTensor(new[, ,] { { { 1, 4, 9 }, { 16, 25, 36 } }, { { 49, 64, 81 }, { 100, 121, 144 } } });
+            var expected = GenTensor<int, IntWrapper>.CreateTensor(new[,,] { { { 1, 4, 9 }, { 16, 25, 36 } }, { { 49, 64, 81 }, { 100, 121, 144 } } });
             Assert.AreEqual(expected, tensor);
         }
 
